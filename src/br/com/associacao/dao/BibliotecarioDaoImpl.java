96000a0c9dbcf7c469e12d6ba12bbdd9c51e9d05
Titione <Titione@DESKTOP-58CVG31>
2020-12-14 20:12:54 -0200

    Exercicio finalizado.

--- src/br/com/associacao/dao/BibliotecarioDaoImpl.java
+++ src/br/com/associacao/dao/BibliotecarioDaoImpl.java
@@ -5,6 +5,7 @@
  */
 package br.com.associacao.dao;
 
+import br.com.associacao.entidade.Bibliotecario;
 import br.com.associacao.entidade.Usuario;
 import java.io.Serializable;
 import java.sql.Connection;
@@ -24,49 +25,49 @@ public class BibliotecarioDaoImpl implements Serializable {
     protected PreparedStatement preparando;
     protected ResultSet resultSet;
 
-    public void salvar(Usuario usuario) throws SQLException {
-        String sql = "INSERT INTO usuarios (cdUsuario, Nome, Login, Senha, "
+    public void salvar(Bibliotecario bibliotecario) throws SQLException {
+        String sql = "INSERT INTO bibliotecario (cdBibliotecario, Login, Senha, Nome, "
                 + "Logradouro, Cidade, Estado, CEP) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
         try {
             conexao = FabricaConexao.abrirConexao();
             preparando = conexao.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            preparando.setInt(1, usuario.getCdusuario());
-            preparando.setString(2, usuario.getNome());
-            preparando.setString(3, usuario.getLogin());
-            preparando.setString(4, usuario.getSenha());
-            preparando.setString(5, usuario.getLogradouro());
-            preparando.setString(6, usuario.getCidade());
-            preparando.setString(7, usuario.getEstado());
-            preparando.setString(8, usuario.getCep());
+            preparando.setInt(1, bibliotecario.getCdBibliotecario());
+            preparando.setString(2, bibliotecario.getNome());
+            preparando.setString(3, bibliotecario.getLogin());
+            preparando.setString(4, bibliotecario.getSenha());
+            preparando.setString(5, bibliotecario.getLogradouro());
+            preparando.setString(6, bibliotecario.getCidade());
+            preparando.setString(7, bibliotecario.getEstado());
+            preparando.setString(8, bibliotecario.getCep());
             preparando.executeUpdate();
         } catch (SQLException e) {
-            System.err.println("Erro ao salvar Usuario " + e.getMessage());
+            System.err.println("Erro ao salvar bibliotecario " + e.getMessage());
         }
     }
 
-    public void alterar(Usuario usuario) throws SQLException {
-        String sql = "UPDATE Usuarios SET Nome = ?, Login = ?, Senha = ?,"
+    public void alterar(Bibliotecario bibliotecario)throws SQLException {
+        String sql = "UPDATE bibliotecario SET Nome = ?, Login = ?, Senha = ?,"
                 + "Logradouro = ?, Cidade = ?, Estado= ?, CEP = ?";
         try {
             conexao = FabricaConexao.abrirConexao();
-            preparando.setString(1, usuario.getNome());
-            preparando.setString(2, usuario.getLogin());
-            preparando.setString(3, usuario.getSenha());
-            preparando.setString(4, usuario.getLogradouro());
-            preparando.setString(5, usuario.getCidade());
-            preparando.setString(6, usuario.getEstado());
-            preparando.setString(7, usuario.getCep());
+            preparando.setString(1, bibliotecario.getNome());
+            preparando.setString(2, bibliotecario.getLogin());
+            preparando.setString(3, bibliotecario.getSenha());
+            preparando.setString(4, bibliotecario.getLogradouro());
+            preparando.setString(5, bibliotecario.getCidade());
+            preparando.setString(6, bibliotecario.getEstado());
+            preparando.setString(7, bibliotecario.getCep());
             preparando.executeUpdate();
 
         } catch (SQLException e) {
-            System.err.println("Erro ao alterar Usuario " + e.getMessage());
+            System.err.println("Erro ao alterar bibliotecario " + e.getMessage());
         }
     }
 
     public void excluir(Integer id) throws SQLException {
         try {
             conexao = FabricaConexao.abrirConexao();
-            preparando = conexao.prepareStatement("DELETE FROM Usuarios WHERE  cdUsuario = ?");
+            preparando = conexao.prepareStatement("DELETE FROM bibliotecario WHERE  cdBibliotecario = ?");
             preparando.setInt(1, id);
             preparando.executeUpdate();
         } catch (SQLException e) {
@@ -76,27 +77,27 @@ public class BibliotecarioDaoImpl implements Serializable {
         }
     }
 
-    public Usuario pesquisarPorId(Integer id) throws SQLException {
-        String consulta = "SELECT * FROM Usuarios WHERE cdUsuario = ?";
-        Usuario usuario = null;
+    public Bibliotecario pesquisarPorId(Integer id) throws SQLException {
+        String consulta = "SELECT * FROM bibliotecario WHERE cdBibliotecario = ?";
+        Bibliotecario bibliotecario = null;
         try {
             conexao = FabricaConexao.abrirConexao();
             preparando = conexao.prepareStatement(consulta);
             preparando.setInt(1, id);
             resultSet = preparando.executeQuery();
-            usuario.setNome(resultSet.getString("Nome"));
-            usuario.setLogin(resultSet.getString("Nogin"));
-            usuario.setSenha(resultSet.getString("Senha"));
-            usuario.setLogradouro(resultSet.getString("Logradouro"));
-            usuario.setCidade(resultSet.getString("Cidade"));
-            usuario.setEstado(resultSet.getString("Estado"));
-            usuario.setCep(resultSet.getString("CEP"));
+            bibliotecario.setNome(resultSet.getString("Nome"));
+            bibliotecario.setLogin(resultSet.getString("Login"));
+            bibliotecario.setSenha(resultSet.getString("Senha"));
+            bibliotecario.setLogradouro(resultSet.getString("Logradouro"));
+            bibliotecario.setCidade(resultSet.getString("Cidade"));
+            bibliotecario.setEstado(resultSet.getString("Estado"));
+            bibliotecario.setCep(resultSet.getString("CEP"));
         } catch (SQLException e) {
             System.err.println("Erro ao pesquisar por id. Erro =  " + e.getMessage());
         } finally {
             FabricaConexao.fecharConexao(conexao, preparando, resultSet);
         }
-        return usuario;
+        return bibliotecario;
     }
 
 }
